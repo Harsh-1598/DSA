a40e593ae22c6a4b11740288425c460d9890f2ad
h.a.r.s.h.1598 <kejriwalharshit123@gmail.com>
2026-05-14 18:19:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.5 MB (69.92%)

---
+++ leetcode questions/0050-powx-n/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (x == 0) return 0;
+        
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double half = myPow(x, N / 2);
+        
+        if (N % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+};
