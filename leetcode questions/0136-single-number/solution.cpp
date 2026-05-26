6b0f74dcb65f9485a1bf77ed5af13ac3e5161b9e
h.a.r.s.h.1598 <kejriwalharshit123@gmail.com>
2026-05-26 17:09:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.7 MB (23.63%)

---
+++ leetcode questions/0136-single-number/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int digit =0;
+        for(int i = 0; i<nums.size(); i++){
+            digit^=nums[i];
+        }
+        return digit;       
+    }
+};
